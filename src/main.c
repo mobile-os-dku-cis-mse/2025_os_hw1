dea69fbfbbb1e269932542e2bd1809687baa399d
32204939_Sa-In HONG <shinythink@gmail.com>
2025-09-29 20:58:42 +0900

    feat: Add read_user_command() function
    This function is for reading user input in REPL.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <limits.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define MAX_CMD_LEN 1024
+#define MAX_PATH_LEN 1024
+#define MAX_ARGS    128
+
+// todo 넘을 때, 예외처리
+char* read_user_command() {
+    static char line[MAX_CMD_LEN];
+    printf("sish> ");
+    fgets(line, sizeof(line), stdin);
+    line[strcspn(line, "\n")] = '\0';
+    return line;
+}
+
 
 int main(int argc, const char * argv[]) {
 
-
     while (1) {
+        char* command_argv[MAX_ARGS];
+        char* executable_path[MAX_PATH_LEN];
 
+        char* command_line = read_user_command();
     }
 }
